70620c15f87a86bd214fce0efafe8ed159ce20ca
Kepins <sz.kepinski1@gmail.com>
2024-05-06 00:08:07 +0200

    API-GATEWAY: Setup project

---
+++ apigateway/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using apigateway.Dtos.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apigateway.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpPost("Login", Name = "PostAuthLogin")]
+    public TokenInfo PostLogin(LoginInfo loginInfo)
+    {
+        return new TokenInfo()
+        {
+            Token = "1234"
+        };
+    }
+}
