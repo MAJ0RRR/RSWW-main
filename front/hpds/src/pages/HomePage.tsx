83f90b0ee467f22c75cc022a59b66c27fc1ad9f6
ola567 <aleksandra.hein@o2.pl>
2024-05-05 12:16:12 +0200

    FRONT: wip

--- front/hpds/src/pages/HomePage.tsx
+++ front/hpds/src/pages/HomePage.tsx
@@ -1,5 +1,12 @@
+import NavBarNotLoggedIn from "../components/NavBarNotLoggedIn";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <NavBarNotLoggedIn />
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
 export default HomePage;
--- front/hpds/src/pages/LogInPage.tsx
+++ front/hpds/src/pages/LogInPage.tsx
@@ -1,5 +1,40 @@
+import NavBarNotLoggedIn from "../components/NavBarNotLoggedIn";
+import "../styles/LogInStyles.css";
+
 function LogInPage() {
-  return <h1>Log in Page</h1>;
+  return (
+    <div>
+      <NavBarNotLoggedIn />
+      <div className="container">
+        <div className="header">Log in</div>
+        <div className="inputs">
+          <div className="input">
+            <label htmlFor="login">Login</label>
+            <input
+              type="text"
+              placeholder="Enter login"
+              name="login"
+              required
+            />
+          </div>
+          <div className="input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              required
+            />
+          </div>
+        </div>
+        <div className="submit-container">
+          <button type="button" className="btn btn-light">
+            Log in
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LogInPage;
