8f40337d97860e31ab849806cd060e74b4368f00
ola567 <aleksandra.hein@o2.pl>
2024-05-07 23:16:12 +0200

    FRONT: reservation css wip

--- front/hpds/src/pages/ReservationPage.tsx
+++ front/hpds/src/pages/ReservationPage.tsx
@@ -67,6 +67,58 @@ function ReservationPage() {
           </div>
           <div className="page-section-content">
             <div className="page-section-content-title">Configuration</div>
+            <div className="page-section-content-content">
+              <div className="user-input">
+                <div className="left">
+                  <label htmlFor="food">Food</label>
+                </div>
+                <div className="right">
+                  220 PLN (per night)
+                  <input type="checkbox" name="food"></input>
+                </div>
+              </div>
+              <div className="user-input-result-one">
+                Rooms total: 220 PLN(per night) x 4 nights = 880 PLN
+              </div>
+              <div className="page-section-content-title">
+                Room configuration
+              </div>
+              <div className="user-input">
+                <div className="left">
+                  <label htmlFor="room_size_1">Size 1</label>
+                </div>
+                <div className="right">
+                  220 PLN (per night)
+                  <input type="text" name="room_size_1"></input>
+                </div>
+              </div>
+              <div className="user-input">
+                <div className="left">
+                  <label htmlFor="room_size_2">Size 2</label>
+                </div>
+                <div className="right">
+                  300 PLN (per night)
+                  <input type="text" name="room_size_2"></input>
+                </div>
+              </div>
+              <div className="user-input">
+                <div className="left">
+                  <label htmlFor="room_size_3">Size 3</label>
+                </div>
+                <div className="right">
+                  420 PLN (per night)
+                  <input type="text" name="room_size_3"></input>
+                </div>
+              </div>
+              <div className="user-input-result-two">
+                <div className="user-input-result-two-left">
+                  Rooms capacity: 6/6
+                </div>
+                <div className="user-input-result-two-right">
+                  Rooms total: 640 PLN(per night) x 4 nights = 2560 PLN
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
