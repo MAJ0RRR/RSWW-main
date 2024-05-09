c5cd0c758761e59a1dc3dba13200b7b78e2bea93
ola567 <aleksandra.hein@o2.pl>
2024-05-09 23:31:49 +0200

    FRONT: SearchResultPage done withouth search bar

--- front/hpds/src/pages/SearchResultPage.tsx
+++ front/hpds/src/pages/SearchResultPage.tsx
@@ -1,10 +1,86 @@
 import NavBarNotLoggedIn from "../components/NavBarNotLoggedIn";
+import Search from "../components/Search";
 
 function SearchResultPage() {
   return (
     <>
       <NavBarNotLoggedIn />
-      <h1>Reservation Page</h1>
+      <Search />
+      <div className="page-content">
+        <div className="page-title">Holidays Poland</div>
+        <div className="page-section-content">
+          <div className="elements">
+            <div className="left-50 font-size-36">Marion</div>
+            <div className="right-50 font-size-36">32321 PLN</div>
+          </div>
+          <div className="elements">
+            <div className="left-50">
+              Address: <br />
+              Transport: <br />
+              Date: <br />
+              Duration: <br />
+            </div>
+            <div className="right-50-relative">
+              <div className="bottom-right">
+                <button
+                  type="button"
+                  className="btn btn-light custom button-style"
+                >
+                  Check offer
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="page-section-content">
+          <div className="elements">
+            <div className="left-50 font-size-36">Marion</div>
+            <div className="right-50 font-size-36">32321 PLN</div>
+          </div>
+          <div className="elements">
+            <div className="left-50">
+              Address: <br />
+              Transport: <br />
+              Date: <br />
+              Duration: <br />
+            </div>
+            <div className="right-50-relative">
+              <div className="bottom-right">
+                <button
+                  type="button"
+                  className="btn btn-light custom button-style"
+                >
+                  Check offer
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="page-section-content">
+          <div className="elements">
+            <div className="left-50 font-size-36">Marion</div>
+            <div className="right-50 font-size-36">32321 PLN</div>
+          </div>
+          <div className="elements">
+            <div className="left-50">
+              Address: <br />
+              Transport: <br />
+              Date: <br />
+              Duration: <br />
+            </div>
+            <div className="right-50-relative">
+              <div className="bottom-right">
+                <button
+                  type="button"
+                  className="btn btn-light custom button-style"
+                >
+                  Check offer
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
