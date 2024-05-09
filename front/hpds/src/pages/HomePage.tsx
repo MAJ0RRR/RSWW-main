4214316e985255d247c15c05831b946c45a5d75e
ola567 <aleksandra.hein@o2.pl>
2024-05-09 23:36:34 +0200

    FRONT: HomePage done without search bar

--- front/hpds/src/pages/HomePage.tsx
+++ front/hpds/src/pages/HomePage.tsx
@@ -6,7 +6,48 @@ function HomePage() {
     <>
       <NavBarNotLoggedIn />
       <Search />
-      <h1>Home Page</h1>
+      <div className="page-content">
+        <div className="page-title">Popular directions</div>
+        <div className="page-section-content">
+          <div className="elements">
+            <div className="left-50 font-size-36">Greece</div>
+            <div className="right-50 font-size-36">
+              <button
+                type="button"
+                className="btn btn-light custom button-style"
+              >
+                Check offers
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="page-section-content">
+          <div className="elements">
+            <div className="left-50 font-size-36">Greece</div>
+            <div className="right-50 font-size-36">
+              <button
+                type="button"
+                className="btn btn-light custom button-style"
+              >
+                Check offers
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="page-section-content">
+          <div className="elements">
+            <div className="left-50 font-size-36">Greece</div>
+            <div className="right-50 font-size-36">
+              <button
+                type="button"
+                className="btn btn-light custom button-style"
+              >
+                Check offers
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
