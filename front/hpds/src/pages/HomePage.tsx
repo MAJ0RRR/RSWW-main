2c619909b94c12410241055e0626e6d65734e25c
ola567 <aleksandra.hein@o2.pl>
2024-05-10 22:43:00 +0200

    FRONT: homepage add loop

--- front/hpds/src/pages/HomePage.tsx
+++ front/hpds/src/pages/HomePage.tsx
@@ -17,36 +17,18 @@ function HomePage() {
       <SearchBar />
       <div className="page-content">
         <div className="page-title">Popular directions</div>
-        <div className="page-section-content">
-          <div className="elements">
-            <div className="left-50 font-size-36">Greece</div>
-            <div className="right-50 font-size-36">
-              <Button variant="secondary" className="font-size-36">
-                Check offers
-              </Button>
+        {mocked_popular_destinations.map((item) => (
+          <div className="page-section-content">
+            <div className="elements">
+              <div className="left-50 font-size-36">{item}</div>
+              <div className="right-50 font-size-36">
+                <Button variant="secondary" className="font-size-36">
+                  Check offers
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="page-section-content">
-          <div className="elements">
-            <div className="left-50 font-size-36">Greece</div>
-            <div className="right-50 font-size-36">
-              <Button variant="secondary" className="font-size-36">
-                Check offers
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className="page-section-content">
-          <div className="elements">
-            <div className="left-50 font-size-36">Greece</div>
-            <div className="right-50 font-size-36">
-              <Button variant="secondary" className="font-size-36">
-                Check offers
-              </Button>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
