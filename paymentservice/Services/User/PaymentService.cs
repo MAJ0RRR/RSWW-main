d90c2bbcb9b0a087751f73708af76aec4bc75b46
Piotr Sieński <s184297@student.pg.edu.pl>
2024-05-18 03:38:59 +0200

    Add payment service

---
+++ paymentservice/Services/User/PaymentService.cs
@@ -0,0 +1,17 @@
+﻿using contracts;
+
+namespace paymentservice.Services.Payment;
+
+public class PaymentService
+{
+
+    private static readonly Random _random = new Random();
+
+    public PayResponse Pay(PayRequest request)
+    {
+        bool paymentResult = _random.Next(0, 10) >= 1; // 90% chance of success
+
+        return new PayResponse(paymentResult);
+    }
+
+}
