4e337ce3a5d24fad186052844afae6ef3c686f05
maj0r <sebastian.lesniewski.edu@gmail.com>
2024-05-18 16:46:34 +0200

    Fill Hotel.cs model

--- contracts/Dtos/HotelDto.cs
+++ contracts/Dtos/HotelDto.cs
@@ -4,7 +4,7 @@ public class HotelDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public Dictionary<int, Tuple<decimal, int>> Rooms { get; set; } // sizeOfRoom -> (price, roomsCount)
+    public Dictionary<int, Tuple<decimal, int>> Rooms { get; set; }
     public List<RoomReservationDto> Bookings { get; set; }
     public AddressDto Address { get; set; }
     public List<DiscountDto> Discounts { get; set; }
--- hotelservice/Models/Hotel.cs
+++ hotelservice/Models/Hotel.cs
@@ -1,10 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace hotelservice.Models;
+﻿namespace hotelservice.Models;
 
 public class Hotel
 {
-    // TODO: fill this
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public Address Address { get; set; }
+    public decimal FoodPricePerPerson { get; set; }
+    public Dictionary<int, (decimal price, int roomsCount)> Rooms { get; set; }
+    public List<Discount> Discounts { get; set; }
+    public List<Rooms> RoomsList { get; set; }
+}
+
+public class RoomReservation
+{
+    public Guid Id { get; set; }
+    public Guid RoomsId { get; set; } // Foreign key
+    public int RoomsReserved { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public DateTime? CancelationDate { get; set; }
+}
+
+public class Address
+{
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string Street { get; set; }
+    public string? ShowName { get; set; }
+}
+
+public class Discount
+{
+    public Guid Id { get; set; }
+    public Guid RoomsId { get; set; } // Foreign key
+    public decimal Value { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+}
+
+public class Rooms
+{
+    public Guid Id;
+    public Guid HotelId; // Foreign key
+    public int Size;
+    public Decimal Price;
+    public int Count;
+    public List<RoomReservation> Bookings { get; set; }
 }
